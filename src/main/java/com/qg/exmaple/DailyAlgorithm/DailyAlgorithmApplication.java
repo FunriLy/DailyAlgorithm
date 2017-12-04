31435dd9cc2d0d49f783b84081fd6f24ccfe8406
FunriLy <fr479910770@qq.com>
2017-12-04 19:47:40 +0800

    init

---
+++ src/main/java/com/qg/exmaple/DailyAlgorithm/DailyAlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.qg.exmaple.DailyAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyAlgorithmApplication.class, args);
+	}
+}
